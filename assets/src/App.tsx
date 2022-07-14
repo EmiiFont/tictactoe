3067c2a4fa964f2eb6db45cef5c82646d2691219
Emilio <emifont01@gmail.com>
2022-07-13 20:46:03 -0400

    feat: better layout for tic tac toe table

--- assets/src/App.tsx
+++ assets/src/App.tsx
@@ -59,34 +59,42 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
-          <div className="flex flex-col">
-              <div className="overflow-x-auto sm:-mx-6 lg:-mx-8">
-                  <div className="py-2 inline-block min-w-full sm:px-6 lg:px-8">
-                      <div className="overflow-hidden">
-                          <table className="hover:table-auto border-separate border-spacing-2 border border-slate-500 min-w-full">
-                              <tbody>
-                              {data.board.map((row: [], i) => (
-                                  <tr key={i} style={trColor}>
-                                      { row.map((col, j) => (
-                                          <td key={j} className="border border-slate-600" style={colorWinner(data.winner, i,j)}
-                                              onClick={(e) => handleClick(i, j)}>
-                                              {
-                                                  data.board[i][j] === 95 ? "_" :
-                                                      mapSymbol(data.board[i][j])
-                                              }
-                                          </td>
-                                      ))}
-                                  </tr>
-                              ))}
-                              </tbody>
-                          </table>
-                      </div>
-                  </div>
-              </div>
-          </div>
-      </header>
-
+        <div className="relative flex min-h-screen flex-col justify-center overflow-hidden bg-gray-50 py-6 bg:py-12">
+            <img src="" alt=""
+                 className="absolute top-1/2 left-1/2 max-w-none -translate-x-1/2 -translate-y-1/2" width="1308"/>
+            <div
+                className="absolute inset-0 bg-center [mask-image:linear-gradient(180deg,white,rgba(255,255,255,0))]"></div>
+            <div
+                className="relative bg-white px-6 pt-10 pb-8 shadow-xl ring-1 ring-gray-900/5 sm:mx-auto sm:max-w-lg sm:rounded-lg sm:px-10">
+                <div className="max-w-md">
+                    <p className="h-6"> Tic Tac Toe </p>
+                    <div className="divide-y divide-gray-300/50">
+                        <div className="space-y-14 py-14 text-gray-600">
+                            <table className="table-fixed">
+                                <tbody>
+                                {data.board.map((row: [], i) => (
+                                    <tr key={i} style={trColor}>
+                                        { row.map((col, j) => (
+                                            <td key={j} className="border w-40 h-40 text-black-700 text-center text-4xl" style={colorWinner(data.winner, i,j)}
+                                                onClick={(e) => handleClick(i, j)}>
+                                                {
+                                                    data.board[i][j] === 95 ? " " :
+                                                        mapSymbol(data.board[i][j])
+                                                }
+                                            </td>
+                                        ))}
+                                    </tr>
+                                ))}
+                                </tbody>
+                            </table>
+                        </div>
+                        <div className="pt-8 text-base font-semibold leading-7">
+                            <button className="bg-sky-500 hover:bg-sky-700 px-5 py-2.5 text-sm leading-5 rounded-md font-semibold text-white">New game</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
   );
 }
